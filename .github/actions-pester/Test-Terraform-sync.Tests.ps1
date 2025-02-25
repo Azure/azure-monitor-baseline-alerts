3df3468a03c758c9c0df0bdc67b051e959fcbd04
Brunoga-MS <bruno.gabrielli@microsoft.com>
2025-02-25 11:54:35 +0100

    Refactor variable names in Terraform sync tests for clarity

--- .github/actions-pester/Test-Terraform-sync.Tests.ps1
+++ .github/actions-pester/Test-Terraform-sync.Tests.ps1
@@ -4,15 +4,15 @@ Describe "UnitTest-CompareEslzTerraform-Sync" {
     Import-Module -Name $PSScriptRoot\PolicyPesterTestHelper.psm1 -Force
 
     # Setting param files to be compared
-    $alzArmParamFile = "./patterns/alz/alzArm.param.json"
+    $alzArmFile = "./patterns/alz/alzArm.param.json"
     $eslzTerraformFile = "./patterns/alz/eslzArm.terraform-sync.param.json"
 
     # Loading file content
-    $alzArmParamJson = Get-Content -Raw -Path $alzArmParamFile | ConvertFrom-Json
+    $alzArmJson = Get-Content -Raw -Path $alzArmFile | ConvertFrom-Json
     $eslzTerraformJson = Get-Content -Raw -Path $eslzTerraformFile | ConvertFrom-Json
 
-    $alzArmParamJsonParams = $alzArmParamJson.parameters | Get-Member -MemberType NoteProperty
-    $eslzTerraformJsonParams = $eslzTerraformJson.parameters | Get-Member -MemberType NoteProperty
+    $alzArmParameters = $alzArmJson.parameters | Get-Member -MemberType NoteProperty
+    $eslzTerraformParameters = $eslzTerraformJson.parameters | Get-Member -MemberType NoteProperty
 
     #$ExcludePolicy = @()
     #$ExcludeParams = @("ALZManagementSubscriptionId", "BYOUserAssignedManagedIdentityResourceId")
@@ -23,10 +23,12 @@ Describe "UnitTest-CompareEslzTerraform-Sync" {
     It "Check param files have the same parameters" {
 
       #Comparing parameter names
-      foreach ($key in $alzArmParamJsonParams) {
-        $paramName = $_.Name
-        Write-Warning "Testing record [$Key] and param name [$paramName] on file [$alzArmParamFile]"
-        $_.Name | Should -Be $eslzTerraformJsonParams.$paramName -Because "the parameter name [$paramName] is not existing in file [eslzArm.terraform-sync.param.json] and must be added."
+      foreach ($key in $alzArmParameters) {
+        if ($_.Name !like "policyAssignmentParameters*") {
+          $paramName = $_.Name
+          Write-Warning "Testing record [$Key] and param name [$eslzTerraformParameters.$paramName] on file [$alzArmFile]"
+          $_.Name | Should -Be $eslzTerraformParameters.$paramName -Because "the parameter name [$paramName] is not existing in file [eslzArm.terraform-sync.param.json] and must be added."
+        }
       }
     }
   }
@@ -35,7 +37,7 @@ Describe "UnitTest-CompareEslzTerraform-Sync" {
     It "Check params have the default values" {
 
       # Comparing parameter values
-      foreach ($key in $alzArmParamJson.parameters) {
+      foreach ($key in $alzArmJson.parameters) {
         $Key | Should -Be $eslzTerraformJson.parameter.$key -Because "The key parameter [$key] is not present on file [eslzArm.terraform-sync.param.json] and must be added."
       }
   }#>
