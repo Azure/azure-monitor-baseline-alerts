c2c875a67142c033fa6a6ea40b71978918765dc0
Brunoga-MS <bruno.gabrielli@microsoft.com>
2025-04-29 17:31:25 +0200

    Refactor alert details table generation in Generate-AlertsDetailsTable.ps1
    - Updated column order in Activity Log alerts table to improve readability.
    - Enhanced Log Search alerts table by adding Evaluation Period and Evaluation Frequency columns.
    - Modified Metric alerts table to include Evaluation Period and Evaluation Frequency, ensuring consistency across alert types.
    - Adjusted data extraction logic to populate new columns for Evaluation Period and Evaluation Frequency from JSON content.
    - Improved regex handling for alert names and scopes to ensure accurate data representation.

--- docs/content/patterns/alz/Getting-started/Activity-Log-Alerts-Table.md
+++ docs/content/patterns/alz/Getting-started/Activity-Log-Alerts-Table.md
@@ -3,22 +3,22 @@ title: ActivityLog alerts table
 geekdocHidden: true
 ---
 
-| Alert Policy Name | Alert Name | Target Resource Type | Alert Scope | Severity | Enabled |
-| ----------------- | ---------- | -------------------- | ----------- | -------- | ------- |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Application Insights Delete Alert | ActivityAppInsightsDelete | microsoft.insights/components | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Managed HSMs Delete Alert | ActivityManagedHSMDelete | microsoft.keyvault/managedHSMs | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Key Vault Delete Alert | ActivityKeyVaultDelete | microsoft.keyvault/vaults | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Azure FireWall Delete Alert | ActivityAzureFirewallDelete | Microsoft.Network/azureFirewalls | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log NSG Delete Alert | ActivityNSGDelete | Microsoft.Network/networkSecurityGroups | subscription | Verbose | true |
-| [Preview]: Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Route Table Delete Alert | ActivityUDRDelete | Microsoft.Network/routeTables | subscription | Verbose | true |
-| [Preview]: Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Routes Delete Alert | ActivityUDRRoutesDelete | Microsoft.Network/routeTables | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Route Table Update Alert | ActivityUDRUpdate | Microsoft.Network/routeTables | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log VPN Gateway Delete Alert | ActivityVPNGatewayDelete | Microsoft.Network/vpnGateways | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log LA Workspace Delete Alert | ActivityLAWorkspaceDelete | Microsoft.OperationalInsights/workspaces | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log LA Workspace Regenerate Key Alert | ActivityLAWorkspaceRegenKey | Microsoft.OperationalInsights/workspaces | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Resource Health Unhealthy Alert | ResourceHealthUnhealthyAlert | Microsoft.Resources/subscriptions | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Advisory Alert | ServiceHealthAdvisoryEvent | Microsoft.Resources/subscriptions | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Incident Alert | ServiceHealthIncident | Microsoft.Resources/subscriptions | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Maintenance Alert | ServiceHealthPlannedMaintenance | Microsoft.Resources/subscriptions | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Security Advisory Alert | ServiceHealthSecurityIncident | Microsoft.Resources/subscriptions | subscription | Verbose | true |
-| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Storage Account Delete Alert | Activity Log Storage Account Delete | Microsoft.Storage/storageAccounts | subscription | Verbose | true |
+| Alert Policy Name | Alert Name | Alert Scope | Target Resource Type | Severity | Enabled |
+| ----------------- | ---------- | ----------- | -------------------- | -------- | ------- |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Application Insights Delete Alert | ActivityAppInsightsDelete | subscription | microsoft.insights/components | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Managed HSMs Delete Alert | ActivityManagedHSMDelete | subscription | microsoft.keyvault/managedHSMs | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Key Vault Delete Alert | ActivityKeyVaultDelete | subscription | microsoft.keyvault/vaults | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Azure FireWall Delete Alert | ActivityAzureFirewallDelete | subscription | Microsoft.Network/azureFirewalls | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log NSG Delete Alert | ActivityNSGDelete | subscription | Microsoft.Network/networkSecurityGroups | Verbose | true |
+| [Preview]: Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Route Table Delete Alert | ActivityUDRDelete | subscription | Microsoft.Network/routeTables | Verbose | true |
+| [Preview]: Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Routes Delete Alert | ActivityUDRRoutesDelete | subscription | Microsoft.Network/routeTables | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Route Table Update Alert | ActivityUDRUpdate | subscription | Microsoft.Network/routeTables | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log VPN Gateway Delete Alert | ActivityVPNGatewayDelete | subscription | Microsoft.Network/vpnGateways | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log LA Workspace Delete Alert | ActivityLAWorkspaceDelete | subscription | Microsoft.OperationalInsights/workspaces | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log LA Workspace Regenerate Key Alert | ActivityLAWorkspaceRegenKey | subscription | Microsoft.OperationalInsights/workspaces | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Resource Health Unhealthy Alert | ResourceHealthUnhealthyAlert | subscription | Microsoft.Resources/subscriptions | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Advisory Alert | ServiceHealthAdvisoryEvent | subscription | Microsoft.Resources/subscriptions | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Incident Alert | ServiceHealthIncident | subscription | Microsoft.Resources/subscriptions | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Maintenance Alert | ServiceHealthPlannedMaintenance | subscription | Microsoft.Resources/subscriptions | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Service Health Security Advisory Alert | ServiceHealthSecurityIncident | subscription | Microsoft.Resources/subscriptions | Verbose | true |
+| Deploy Azure Monitor Baseline Alerts (AMBA-ALZ) - Activity Log Storage Account Delete Alert | Activity Log Storage Account Delete | subscription | Microsoft.Storage/storageAccounts | Verbose | true |
--- tooling/ALZ/Generate-AlertsDetailsTable.ps1
+++ tooling/ALZ/Generate-AlertsDetailsTable.ps1
@@ -23,8 +23,8 @@ $metricAlertTableFile = $alertTablesRoorDir + "\Metric-Alerts-Table.md"
 "geekdocHidden: true" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
 "---" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append -NoNewline
 "`n" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
-"| Alert Policy Name | Alert Name | Target Resource Type | Alert Scope | Severity | Enabled |" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
-"| ----------------- | ---------- | -------------------- | ----------- | -------- | ------- |" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
+"| Alert Policy Name | Alert Name | Alert Scope | Target Resource Type | Severity | Enabled |" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
+"| ----------------- | ---------- | ----------- | -------------------- | -------- | ------- |" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
 
 # appending lines to Log Search source table files
 "---" | Out-File $LogSearchAlertTableFile -Encoding UTF8
@@ -32,8 +32,8 @@ $metricAlertTableFile = $alertTablesRoorDir + "\Metric-Alerts-Table.md"
 "geekdocHidden: true" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
 "---" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append -NoNewline
 "`n" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
-"| Alert Policy Name | Alert Name | Target Resource Type | Alert Scope | Operator | Threshold | Severity | Enabled |" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
-"| ----------------- | ---------- | -------------------- | ----------- | -------- | --------- | -------- | ------- |" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
+"| Alert Policy Name | Alert Name | Alert Scope | Target Resource Type | Evaluation Period | Evaluation Frequency | Operator | Threshold | Severity | Enabled |" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
+"| ----------------- | ---------- | ----------- | -------------------- | ----------------- | -------------------- |--------- | --------- | -------- | ------- |" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
 
 # appending lines to Metric source table files
 "---" | Out-File $metricAlertTableFile -Encoding UTF8
@@ -41,8 +41,8 @@ $metricAlertTableFile = $alertTablesRoorDir + "\Metric-Alerts-Table.md"
 "geekdocHidden: true" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
 "---" | Out-File $metricAlertTableFile -Encoding UTF8 -Append -NoNewline
 "`n" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
-"| Alert Policy Name | Alert Name | Target Resource Type | Alerts Scope | Metric | Aggregation | Operator | Threshold | Severity | Enabled |" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
-"| ----------------- | ---------- | -------------------- | ------------ | ------ | ----------- | -------- | --------- | -------- | ------- |" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
+"| Alert Policy Name | Alert Name | Alerts Scope | Target Resource Type | Evaluation Period | Evaluation Frequency | Metric | Aggregation | Operator | Threshold | Severity | Enabled |" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
+"| ----------------- | ---------- | ------------ | -------------------- | ----------------- | -------------------- | ------ | ----------- | -------- | --------- | -------- | ------- |" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
 
 
 # Get all JSON files under the root directory and its subdirectories
@@ -59,6 +59,8 @@ foreach ($file in $jsonFiles) {
     $alertName = $null
     $alertScope = $null
     $targetResourceType = $null
+    $evaluationPeriod = $null
+    $evaluationFrequency = $null
     $metric = $null
     $aggregation = $null
     $operator = $null
@@ -73,11 +75,13 @@ foreach ($file in $jsonFiles) {
     $alertType = $jsonContent.properties.policyRule.then.details.type
 
     switch ($alertType) {
+
       # Activitiy Log alerts' source file
       "Microsoft.Insights/activityLogAlerts" {
 
         # Process the JSON content
         $policyName = $jsonContent.properties.displayName
+
         if ($jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.count -eq 1) {
           $alertName = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.name
           $alertScope = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.scopes
@@ -91,53 +95,72 @@ foreach ($file in $jsonFiles) {
         if ([regex]::Matches($alertScope, '(\b\w+\b)').Success) {
           $alertScope = [regex]::Matches($alertScope, '(\b\w+\b)').Groups[1].Value
         }
+
         $targetResourceType = $jsonContent.properties.policyRule.if.allOf[0].equals
         $severity = $severityMapping["4"]
         $enabled = $jsonContent.properties.parameters.enabled.defaultValue.ToString()
 
         # Appending the content to the file
-        "| $policyName | $alertName | $targetResourceType | $alertScope | $severity | $enabled |" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
+        "| $policyName | $alertName | $alertScope | $targetResourceType | $severity | $enabled |" | Out-File $activityLogAlertTableFile -Encoding UTF8 -Append
+
       }
 
-      #Log-Search alerts' source file
+      # Log-Search alerts' source file
       "Microsoft.Insights/scheduledQueryRules" {
 
         # Process the JSON content
         $policyName = $jsonContent.properties.displayName
+
         if ($jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.count -eq 1) {
           $alertName = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.name
+
           if ([regex]::Matches($alertName, '(\b\w+\b)').Success) {
             $alertName = [regex]::Matches($alertName, '(\b\w+\b)').Groups[5].Value + "-" + [regex]::Matches($alertName, '(\b\w+\b)').Groups[6].Value
+
           }
+
           $alertScope = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.scopes
+
         }
         else {
           $alertName = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources[1].properties.template.resources.name
+
           if ([regex]::Matches($alertName, '(\b\w+\b)').Success) {
             $alertName = [regex]::Matches($alertName, '(\b\w+\b)').Groups[3].Value + "-" + [regex]::Matches($alertName, '(\b\w+\b)').Groups[6].Value
+
           }
+
           $alertScope = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources[1].properties.template.resources.properties.scopes
+
         }
+
         $targetResourceType = $jsonContent.properties.policyRule.if.allOf[0].equals
+
         if ([regex]::Matches($alertScope, '(\w+)').Success) {
           if ($file.Name -like "deploy-laworkspace-daily*") {
             $alertScope = [regex]::Matches($alertScope, '(\w+)').Groups[2].Value
+
           }
           elseif ($file.Name -like "deploy-AppInsightsThrottling*") {
             $alertScope = [regex]::Matches($alertScope, '(\w+)').Groups[4].Value + "-" + [regex]::Matches($alertScope, '(\w+)').Groups[12].Value
+
           }
           else {
             $alertScope = [regex]::Match($alertScope, '(\w+)').Groups[1].Value
+
           }
         }
 
+        $evaluationPeriod = $jsonContent.properties.parameters.windowSize.defaultValue
+        $evaluationFrequency = $jsonContent.properties.parameters.evaluationFrequency.defaultValue
         $operator = $jsonContent.properties.parameters.operator.defaultValue
         $threshold = $jsonContent.properties.parameters.threshold.defaultValue
         $severity = $severityMapping[$jsonContent.properties.parameters.severity.defaultValue]
         $enabled = $jsonContent.properties.parameters.enabled.defaultValue.ToString()
 
         # Appending the content to the file
-        "| $policyName | $alertName | $targetResourceType | $alertScope | $operator | $threshold | $severity | $enabled |" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
+        "| $policyName | $alertName | $alertScope | $targetResourceType | $evaluationPeriod | $evaluationFrequency | $operator | $threshold | $severity | $enabled |" | Out-File $LogSearchAlertTableFile -Encoding UTF8 -Append
+
       }
 
       #Metric alerts' source file
@@ -146,42 +169,58 @@ foreach ($file in $jsonFiles) {
         # Process the JSON content
         $policyName = $jsonContent.properties.displayName
         $alertName = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.name
+
         if ([regex]::Matches($alertName, '(\b\w+\b)').Success) {
           $alertName = [regex]::Matches($alertName, '(\b\w+\b)').Groups[4].Value + "-" + [regex]::Matches($alertName, '(\b\w+\b)').Groups[6].Value
+
         }
-        #$targetResourceType = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.criteria.allOf.metricNameSpace
+
         $targetResourceType = $jsonContent.properties.policyRule.if.allOf[0].equals
         $alertScope = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.scopes
+
         if ([regex]::Matches($alertScope, '(\b\w+\b)').Success) {
           $alertScope = [regex]::Matches($alertScope, '(\b\w+\b)').Groups[2].Value
+
         }
+
         $metric = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.criteria.allOf.metricName
         $aggregation = $jsonContent.properties.parameters.aggregation.defaultValue
+
         if ([string]::IsNullOrEmpty($aggregation)) {
           $aggregation = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.criteria.allOf.timeAggregation
+
         }
+
         if ([string]::IsNullOrEmpty($operator)) {
           $operator = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.criteria.allOf.operator
+
         }
         $threshold = $jsonContent.properties.parameters.threshold.defaultValue
+
         if ([string]::IsNullOrEmpty($threshold)) {
           $threshold = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.criteria.allOf.threshold
+
           if ([string]::IsNullOrEmpty($threshold)) {
             $threshold = $jsonContent.properties.policyRule.then.details.deployment.properties.template.resources.properties.criteria.allOf.criterionType
+
           }
         }
+
+        $evaluationPeriod = $jsonContent.properties.parameters.windowSize.defaultValue
+        $evaluationFrequency = $jsonContent.properties.parameters.evaluationFrequency.defaultValue
         $severity = $severityMapping[$jsonContent.properties.parameters.severity.defaultValue]
         $enabled = $jsonContent.properties.parameters.enabled.defaultValue.ToString()
 
         # Appending the content to the file
-        "| $policyName | $alertName | $targetResourceType | $alertScope | $metric | $aggregation | $operator | $threshold | $severity | $enabled |" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
+        "| $policyName | $alertName | $alertScope | $targetResourceType | $evaluationPeriod | $evaluationFrequency | $metric | $aggregation | $operator | $threshold | $severity | $enabled |" | Out-File $metricAlertTableFile -Encoding UTF8 -Append
+
       }
-
-
-
     }
   }
+
   catch {
     Write-Error "Failed to process file: $($file.FullName). Error: $_"
+
   }
+
 }
