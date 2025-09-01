31ca7059ed61292a307a58821c859c47bb6ecb12
Brunoga-MS <bruno.gabrielli@microsoft.com>
2025-09-01 11:03:23 +0200

    Update excluded parameters in policy validation tests

--- .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
+++ .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
@@ -183,7 +183,7 @@ Describe 'UnitTest-ModifiedPolicies' {
         $PolicyFile = Split-Path $_ -Leaf
         $PolicyMetadataName = $PolicyJson.name
         $ExcludePolicy = @()
-        $ExcludeParams = @("ALZManagementSubscriptionId", "ALZMonitorActionGroupEmail", "BYOUserAssignedManagedIdentityResourceId", "UAMIResourceId", "BYOActionGroup", "BYOAlertProcessingRule")
+        $ExcludeParams = @("ALZManagementSubscriptionId", "ALZMonitorActionGroupEmail", "ALZArmRoleId", "BYOUserAssignedManagedIdentityResourceId", "UAMIResourceId", "BYOActionGroup", "BYOAlertProcessingRule")
         if ($PolicyMetadataName -notin $ExcludePolicy) {
           $PolicyParameters = $PolicyJson.properties.parameters
           if ($PolicyParameters | Get-Member -MemberType NoteProperty) {
