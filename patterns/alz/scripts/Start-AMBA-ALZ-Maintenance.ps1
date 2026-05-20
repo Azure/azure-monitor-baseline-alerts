7d6aaa5f183c425980fd5be5d80866f11c7351e1
Brunoga-MS <bruno.gabrielli@microsoft.com>
2026-05-20 16:16:29 +0200

    Update deployment name filter in Get-ALZ-Deployments function to match 'amba-alz-' prefix for improved accuracy

--- patterns/alz/scripts/Start-AMBA-ALZ-Maintenance.ps1
+++ patterns/alz/scripts/Start-AMBA-ALZ-Maintenance.ps1
@@ -322,7 +322,7 @@ Function Get-ALZ-Deployments {
   # get deployments to delete
   $allDeployments = @()
   ForEach ($mg in $managementGroups) {
-    $deployments = Get-AzManagementGroupDeployment -ManagementGroupId "$($mg.mgName)" -WarningAction silentlyContinue | Where-Object { $_.DeploymentName.StartsWith("amba-") }
+    $deployments = Get-AzManagementGroupDeployment -ManagementGroupId "$($mg.mgName)" -WarningAction silentlyContinue | Where-Object { $_.DeploymentName.StartsWith("amba-alz-") }
     $allDeployments += $deployments
   }
   Write-Host "- Found '$($allDeployments.Count)' deployments for AMBA-ALZ pattern with name starting with 'amba-alz-' performed on the '$pseudoRootManagementGroup' Management Group hierarchy." -ForegroundColor Cyan
